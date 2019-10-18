26deed562260230cf654f2738ce7cb483d912dee
Pham Hoang Viet <45876272+hoangviet290398@users.noreply.github.com>
2019-10-18 14:50:07 +0700

    upload project

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '1032586259107-kpb0pnceeuvhilrgpjg3efjn0f8rhbgu.apps.googleusercontent.com',
+            clientSecret: 'pbEf61uDEpz6F9v_m6v0Bodp'
+        },
+        mongodb: {
+            dbURI: 'mongodb://127.0.0.1:27017/sportwebsite'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
