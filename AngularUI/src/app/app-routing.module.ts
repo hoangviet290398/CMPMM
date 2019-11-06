78780c1af99a4e2358c540114fc4b8878f04587a
Quy Tran <quytrandev@gmail.com>
2019-11-06 14:38:39 +0700

    update giao dien

--- AngularUI/src/app/app-routing.module.ts
+++ AngularUI/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BodyComponent } from './body/body.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:BodyComponent},
+  {path:'login',component:LoginComponent},
+  {path:'product-detail',component:ProductDetailComponent},
+  {path:'cart',component:CartComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginComponent,BodyComponent,PageNotFoundComponent,ProductDetailComponent,CartComponent]
--- AngularUI/src/app/app.module.ts
+++ AngularUI/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
-  ],
+    routingComponents,
+    
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
